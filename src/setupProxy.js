83888921d17abf56272e2c632fd179eac607f67f
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-10-02 15:55:51 -0400

    this might work

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,13 +3,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
     createProxyMiddleware("/api/user",{
-      target:"https://nerd-songwriter-backend.onrender.com",
+      target:"https://nerd-songwriter-backend.vercel.app/",
       changeOrigin: true
     })
   );
   app.use(
     createProxyMiddleware("/api/songs",{
-      target:"https://nerd-songwriter-backend.onrender.com",
+      target:"https://nerd-songwriter-backend.vercel.app/",
       changeOrigin: true
     })
   );
