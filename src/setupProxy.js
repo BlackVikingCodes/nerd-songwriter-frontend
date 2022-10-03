4ce3ec63181dcfb71670c60fa130fd6e65c89b3c
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-10-03 17:58:24 -0400

    trying fly.io

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,19 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
     createProxyMiddleware("/api/user/login",{
-      target:"https://nerd-songwriter-backend.vercel.app/",
+      target:"nerd-songwriter-api.fly.dev",
       changeOrigin: true
     })
   );
   app.use(
     createProxyMiddleware("/api/user/signup",{
-      target:"https://nerd-songwriter-backend.vercel.app/",
+      target:"nerd-songwriter-api.fly.dev",
       changeOrigin: true
     })
   );
   app.use(
     createProxyMiddleware("/api/songs",{
-      target:"https://nerd-songwriter-backend.vercel.app/",
+      target:"nerd-songwriter-api.fly.dev",
       changeOrigin: true
     })
   );
