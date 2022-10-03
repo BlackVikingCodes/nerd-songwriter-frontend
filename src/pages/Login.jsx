aa88752a1f4ecd646b773ffb7a7b67a94f1992ba
Alfredo Cabrera <alfredocabrera.tech@gmail.com>
2022-10-03 18:52:42 -0400

    regex

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,12 +4,21 @@ import { useLogin } from "../hooks/useLogin"
 const Login = () => {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
+  const [strongPassword, setStrongPassword] = useState(false)
   const {login, error, isLoading} = useLogin()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     await login(userName, password)
+  }
+
+  const handlePassword = (e) => {
+    let regexPass = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/
+    if(regexPass.test(e.target.value)){
+      setStrongPassword(true)
+      setPassword(e.target.value)
+    }
   }
 
   return (
@@ -25,11 +34,21 @@ const Login = () => {
       <label>Password:</label>
       <input 
         type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
+        onChange={handlePassword} 
         value={password} 
       />
+      {!strongPassword && 
+        <div>
+          <p>Your password isn't strong enough. <br/> Make sure to have:</p>
+          <ul>
+            <li>At least one uppercase letter</li>
+            <li>At least one number</li>
+            <li>At least one symbol</li>
+          </ul>
+        </div>
+      }
 
-      <button className="btn btn-login" disabled={isLoading}>Log in</button>
+      <button className="btn btn-login" disabled={isLoading||!strongPassword}>Log in</button>
       {error && <div className="error">{error}</div>}
     </form>
   )
